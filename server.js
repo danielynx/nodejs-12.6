f4e8ba0cfa04e2610d82cfd7a0931145f949f99c
Daniel Backes <danielbackes@protonmail.com>
2019-07-25 20:31:59 -0400

    Config docker swarm cluster to run nodejs, portainer and traefik

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
-var http = require('http');
+const os = require('os');
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(80);
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(morgan(app.get('env') === 'production' ? 'combined' : 'dev'));
+app.use(helmet());
+app.use(compression());
+
+app.get('/', (req, res) => {
+  res.json({
+    ...req.headers,
+    hostname: os.hostname(),
+    date: new Date().toISOString()
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Server is listening on port', PORT);
+});
+
+process.on('uncaughtException', err => {
+  console.error(err.stack);
+  process.exit(1);
+});
